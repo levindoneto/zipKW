228ae9ef7eab44496b9e6c131ae0c1109ae43e45
Levindo Neto <levindogtn@gmail.com>
2018-02-01 22:43:31 +0100

    Add creating highlighted statement

--- Search.py
+++ Search.py
@@ -6,3 +6,14 @@ def findKeyword(token, listWords):
 
 def getNameFileKW(zipName, lineNumber, keyWord):
     return zipName.split(".")[0] + "_line" + str(lineNumber) + "_" + keyWord + ".kwd"
+
+def createHighlightedStatement(listWords, foundKeyword):
+    statament = ""
+    for i in range(len(listWords)):
+        singleWord = ""
+        if (listWords[i] == foundKeyword):
+            singleWord = "**" + foundKeyword + "**"
+        else:
+            singleWord = listWords[i]
+        statament += singleWord + " "
+    return statament
