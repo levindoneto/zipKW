e87fe144afbaaf35941baee0384de17c338b66b2
Levindo Neto <levindogtn@gmail.com>
2018-02-01 22:39:22 +0100

    Add getting name for a new file of keywords

--- Search.py
+++ Search.py
@@ -3,3 +3,6 @@ def getTokens(statament):
 
 def findKeyword(token, listWords):
     return token in listWords
+
+def getNameFileKW(zipName, lineNumber, keyWord):
+    return zipName.split(".")[0] + "_line" + str(lineNumber) + "_" + keyWord + ".kwd"
