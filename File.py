d1cf2e647e50665e961ca30ed10a18915edcf83e
Levindo Neto <levindogtn@gmail.com>
2018-02-01 22:46:25 +0100

    Add creating file with highlighted keywords

--- File.py
+++ File.py
@@ -2,3 +2,8 @@ def readFile(fileInput):
     with open(fileInput) as f:
         listLines = f.readlines() # List of lines
         return listLines
+
+def createFileKW (filename, highlightedStatament):
+    fileKW = open(filename, "w+")
+    fileKW.write(highlightedStatament)
+    fileKW.close()
