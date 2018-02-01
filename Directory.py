382d40fb8e6420d63f6300109c0049409c4232ab
Levindo Neto <levindogtn@gmail.com>
2018-02-01 22:53:21 +0100

    Add recursively traversing into directories

--- Directory.py
+++ Directory.py
@@ -3,3 +3,9 @@ import ManagementFile
 
 def getRootDirName(zipFile):
     return zipFile.split(".")[0] + "-data"
+
+def walk(rootZip, rootDir, keywords):
+    for root, dirs, files in os.walk(rootDir):
+        for file in files:
+            if file.endswith(".txt"):
+                ManagementFile.manager(rootZip, os.path.join(root, file), keywords)
